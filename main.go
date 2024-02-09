9d0e7cd23d0ac491b19d9b689f4a41d305565a87
Your Namemelvin <melvin.jahsi@ynov.com>
2024-02-09 12:47:29 +0100

    telechargement des images

--- main.go
+++ main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
-	"fmt"
 )
 
-var index = template.Must(template.ParseFiles("html/index.html"))
-var game = template.Must(template.ParseFiles("html/game.html"))
-var win = template.Must(template.ParseFiles("html/index.html"))
-var loose = template.Must(template.ParseFiles("html/index.html"))
-var equality = template.Must(template.ParseFiles("html/index.html"))
-var rules = template.Must(template.ParseFiles("html/index.html"))
+var index = template.Must(template.ParseFiles("tmpl/index.html"))
+var game = template.Must(template.ParseFiles("tmpl/game.html"))
+var win = template.Must(template.ParseFiles("tmpl/index.html"))
+var loose = template.Must(template.ParseFiles("tmpl/index.html"))
+var equality = template.Must(template.ParseFiles("tmpl/index.html"))
+var rules = template.Must(template.ParseFiles("tmpl/index.html"))
+var pseudo = template.Must(template.ParseFiles("tmpl/pseudo.html"))
 
+type tictactoe struct{
+	pseudo		string
+	win			bool
+	equality	bool
+	endgame		bool
+}
 
 const port = ":3000"
 
@@ -29,34 +36,50 @@ func main(){
 
 	http.HandleFunc("/rules", Rules)
 
-	http.HandleFunc("/win", Win)
+	http.HandleFunc("/pseudo", Pseudo)
 
 	fmt.Println("Serveur en cours d'execution sur http://localhost:8080")
 	http.ListenAndServe(port, nil)
 }
 
 func Index (w http.ResponseWriter, r*http.Request){
-	renderTemplate(w, "html/index")
+	renderTemplate(w, "tmpl/index")
 }
 
 func Game (w http.ResponseWriter, r*http.Request){
-	renderTemplate(w, "html/game")
+	renderTemplate(w, "tmpl/game")
+}
+
+func Pseudo(w http.ResponseWriter, r *http.Request) {
+	var data string
+	pseudo := r.FormValue("pseudo")
+
+	if len(pseudo) > 0 {
+		// Mettre à jour la variable pseudo
+
+		data.Pseudo = pseudo
+
+		http.Redirect(w, r, "/skin", http.StatusSeeOther)
+	}
+
+	renderTemplate(w, "tmpl/pseudo")
+
 }
 
 func Win (w http.ResponseWriter, r*http.Request){
-	renderTemplate(w, "html/win")
+	renderTemplate(w, "tmpl/win")
 }
 
 func Loose (w http.ResponseWriter, r*http.Request){
-	renderTemplate(w, "html/loose")
+	renderTemplate(w, "tmpl/loose")
 }
 
 func Equality (w http.ResponseWriter, r*http.Request){
-	renderTemplate(w, "html/equality")
+	renderTemplate(w, "tmpl/equality")
 }
 
 func Rules (w http.ResponseWriter, r*http.Request){
-	renderTemplate(w, "html/rules")
+	renderTemplate(w, "tmpl/rules")
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string) { //Parse le fichier html et envoi les informations au client
@@ -67,3 +90,4 @@ func renderTemplate(w http.ResponseWriter, tmpl string) { //Parse le fichier htm
 	}
 	t.Execute(w, nil)
 }
+
